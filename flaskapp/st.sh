404afccc6882b9a1891eb233159261d047a9ecd3
dsalato <d.salato@mail.ru>
2025-04-12 14:36:22 +0700

    add gunicorn dependencies

--- flaskapp/st.sh
+++ flaskapp/st.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-# Запускаем сервер в фоне
 gunicorn --bind 127.0.0.1:5000 wsgi:app & APP_PID=$!
-sleep 10  # Даем серверу больше времени на запуск
+sleep 10
 
-# Запускаем тесты
 echo "Starting client tests..."
 python3 client.py
 APP_CODE=$?
 
-# Останавливаем сервер
 echo "Stopping server..."
 kill -TERM $APP_PID
 wait $APP_PID
 
-# Выходим с кодом клиента
 echo "Client exit code: $APP_CODE"
 exit $APP_CODE
