2693f56193502c70e06a505caf9528503994bd19
dsalato <d.salato@mail.ru>
2025-04-12 15:26:12 +0700

    fix railway

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -149,4 +149,5 @@ def apixml():
         return f"Error processing XML: {str(e)}", 500
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
